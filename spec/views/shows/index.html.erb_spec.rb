51cbea5f1935fcd91ff46a37092fc2ca1c26edf3
jotran <josephthientran@gmail.com>
2016-11-08 01:02:51 -0800

    Update the shows index view and app layout with a new design
    Update spec testing the shows index view.

--- spec/views/shows/index.html.erb_spec.rb
+++ spec/views/shows/index.html.erb_spec.rb
@@ -6,24 +6,23 @@ RSpec.describe "shows/index", type: :view do
       Show.create!(
         :tvdb_id => "Tvdb",
         :name => "Name",
-        :season => 2,
-        :episode => 3
+        :season => "2",
+        :episode => "3"
       ),
       Show.create!(
         :tvdb_id => "Tvdb2",
         :name => "Name",
-        :season => 2,
-        :episode => 3
+        :season => "2",
+        :episode => "3"
       )
     ])
   end
 
   it "renders a list of shows" do
     render
-    assert_select "tr>td", :text => "Tvdb".to_s, :count => 1
-    assert_select "tr>td", :text => "Tvdb2".to_s, :count => 1
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
-    assert_select "tr>td", :text => 2.to_s, :count => 2
-    assert_select "tr>td", :text => 3.to_s, :count => 2
+    assert_select "div>em", :text => "2", :count => 2
+    assert_select "div>em", :text => "3", :count => 2
+    assert_select "div>em", :text => "false".to_s, :count => 2
+    assert_select "div>h1", :text => "Name".to_s, :count => 2
   end
 end
