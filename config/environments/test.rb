8fa99b2b61469274343f64d1a8499bf42cea19a9
jotran <josephthientran@gmail.com>
2016-12-13 22:23:36 -0800

    Disable mailer when running tests

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,6 +33,8 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  # Don't perform deliveries when testing.
+  config.action_mailer.perform_deliveries = false
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
